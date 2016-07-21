c0d947481080b28a731ba9c559e379e4c9a55c8d
BalaSubrahmanya Sharma Vedam <subramanyasharma101@gmail.com>
2016-07-22 01:32:30 +0530

    Add files via upload

---
+++ happycartbackend/src/test/java/com/subbu/happycartbackend/SupplierTest.java
@@ -0,0 +1,57 @@
+package com.subbu.happycartbackend;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.subbu.happycartbackend.dao.SupplierDAO;
+import com.subbu.happycartbackend.model.Supplier;
+
+public class SupplierTest {
+	
+	public static void main(String[] args){
+		
+		@SuppressWarnings("resource")
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+		
+		context.scan("com.subbu.happycartbackend");
+		context.refresh();
+		
+		
+		SupplierDAO supplierDAO = (SupplierDAO) context.getBean("supplierDAO");
+		
+		Supplier supplier = (Supplier) context.getBean("supplier");
+		
+		supplier.setId("Supl002");
+		supplier.setName("Apple");
+		supplier.setAddress("California");
+		
+		supplier.setId("Supl001");
+		supplier.setName("Sony");
+		supplier.setAddress("Japan");
+		
+		
+		supplier.setId("Supl003");
+		supplier.setName("Nokia");
+		supplier.setAddress("Finland");
+	
+		
+		supplierDAO.saveOrUpdate(supplier);
+		
+		
+		System.out.println("No.of suppliers available"+supplierDAO.list().size());
+		
+		if( supplierDAO.get("sdfsf")==null)
+		{
+			System.out.println("Supplier doesn't exist");
+			
+		}
+		else
+		{
+			System.out.println("Supplier exist.. no.of suppliers = "+supplierDAO.list().size()+"the details are shown in database only..");
+			System.out.println();
+		}
+		
+	}
+	
+}
+	
+	
